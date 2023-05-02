cabaacaaff5f2fcc77f7c888cf4339df8c5438a9
Priyanshu Ghosh <107946072+PriyanshuGhosh20@users.noreply.github.com>
2023-05-02 18:33:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,34 +6,42 @@ from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
 import validators
 
+# Download the VADER lexicon for sentiment analysis
 nltk.download('vader_lexicon')
 
+# Set the title and icon for the app
 st.set_page_config(page_title="Stock Market Sentiment Analysis", page_icon=":money_with_wings:")
 
+# Display the main title
 st.title("Stock Market Sentiment Analysis")
 
-st.write("This app analyzes the sentiment of financial news headlines to make a stock market prediction.")
-
+# Ask the user for a financial news website URL to analyze
 url = st.text_input("Enter the URL of a financial news website:", placeholder="E.g. https://www.businesstoday.in")
 
+# Allow the user to select the number of headlines to analyze
 num_headlines = st.slider("Select the number of headlines to analyze:", min_value=1, max_value=100, value=10)
 
+# Allow the user to select the type of visualization to display
 visualization_type = st.selectbox("Select the type of visualization to display:",options=["Bar Chart", "Line Chart","Area Chart","Scatter Chart"])
 
+# Analyze the news headlines when the user clicks the "Analyze" button
 try:
     if st.button("Analyze"):
+        # Validate the URL
         if not validators.url(url):
             st.error("Please enter a valid URL.")
         else:
+            # Retrieve the webpage HTML
             with st.spinner('Analyzing the news headlines...'):
                 response = requests.get(url)
                 response.raise_for_status()
                 soup = BeautifulSoup(response.content, 'html.parser')
+                # Find the news headlines and their ticker symbols
                 headlines = soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5'])[:num_headlines]
                 sentiment_scores = []
                 tickers = []
                 analyzer = SentimentIntensityAnalyzer()
-
+                # Analyze the sentiment of each headline and save the scores and ticker symbols
                 for i, headline in enumerate(headlines):
                     text = headline.text
                     ticker = text[:10]
@@ -41,6 +49,7 @@ try:
                     if sentiment_score:
                         tickers.append(ticker)
                         sentiment_scores.append(sentiment_score)
+                # Display the sentiment prediction based on the average score
                 if sentiment_scores:
                     average_score = sum(sentiment_scores) / len(sentiment_scores)
 
@@ -52,51 +61,66 @@ try:
                     else:
                         st.markdown(f"<h2 style='color: yellow;'>The stock market is likely to remain stable.</h2>", unsafe_allow_html=True)
 
+                    # Display the selected type of visualization
                     if visualization_type == "Bar Chart":
+                        # set color map for the plot
                         cmap = plt.cm.get_cmap('RdYlGn')
+                        # normalize the data and set color values
                         normalize = plt.Normalize(vmin=-1, vmax=1)
                         colors = [cmap(normalize(score)) for score in sentiment_scores]
+                        # create the plot
                         fig, ax = plt.subplots(figsize=(25, 10))
                         ax.bar(tickers, sentiment_scores, width=0.5, color=colors)
                         ax.set_xticklabels(tickers, rotation=90)
                         ax.set_xlabel('Tickers')
                         ax.set_ylabel('Sentiment Scores')
+                        # show the plot
                         st.pyplot(fig)
 
                     elif visualization_type == "Line Chart":
+                        # sort the data by ticker
                         sorted_data = sorted(zip(tickers, sentiment_scores), key=lambda x: x[0])
                         sorted_tickers = [data[0] for data in sorted_data]
-                        sorted_scores = [data[1] for data in sorted_data]   
+                        sorted_scores = [data[1] for data in sorted_data]  
+                        # create the plot
                         fig, ax = plt.subplots(figsize=(25, 10))
                         ax.plot(sorted_tickers, sorted_scores, marker='o')
                         ax.set_xticklabels(sorted_tickers, rotation=90)
                         ax.set_xlabel('Tickers')
                         ax.set_ylabel('Sentiment Scores')
+                        # show the plot
                         st.pyplot(fig)
 
                     elif visualization_type == "Area Chart":
+                        # set color map for the plot
                         cmap = plt.cm.get_cmap('RdYlGn')
+                        # normalize the data and set color values
                         normalize = plt.Normalize(vmin=-1, vmax=1)
-                        colors = [cmap(normalize(score)) for score in   sentiment_scores]
+                        colors = [cmap(normalize(score)) for score in sentiment_scores]
+                        # create the plot
                         fig, ax = plt.subplots(figsize=(25, 10))
                         ax.fill_between(tickers, sentiment_scores, 0, color=colors)
                         ax.set_xticklabels(tickers, rotation=90)
                         ax.set_xlabel('Tickers')
                         ax.set_ylabel('Sentiment Scores')
+                        # show the plot
                         st.pyplot(fig)
 
                     elif visualization_type == "Scatter Chart":
+                        # set color map for the plot
                         cmap = plt.cm.get_cmap('RdYlGn')
+                        # normalize the data and set color values
                         normalize = plt.Normalize(vmin=-1, vmax=1)
                         colors = [cmap(normalize(score)) for score in sentiment_scores]
+                        # create the plot
                         fig, ax = plt.subplots(figsize=(25, 10))
                         ax.scatter(tickers, sentiment_scores, color=colors,s=200)
                         ax.set_xticklabels(tickers, rotation=90)
                         ax.set_xlabel('Tickers')
                         ax.set_ylabel('Sentiment Scores')
+                        # show the plot
                         st.pyplot(fig)
                 else:
                     st.write("No headlines found on the page.")
-        
 except Exception as e:
     st.error(f"An error occurred while processing the request: {str(e)}")
